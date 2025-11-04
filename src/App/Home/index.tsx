1907ea34521c1ddef1c59723123cb914ea612997
walenkildely <walenkildely20@gmail.com>
2025-11-03 23:03:11 -0300

    primeiro commit

---
+++ src/App/Home/index.tsx
@@ -0,0 +1,14 @@
+import { View, Image } from "react-native";
+import {styles} from "./styles"
+import { Button } from "@/components/Button"
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style = {styles.logo}/>
+       <Button/>
+    </View>
+
+   
+  )
+}
---
+++ src/App/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+
+    container: {
+        flex : 1,
+        alignItems: "center",
+        backgroundColor: "#d0d2d8",
+        paddingTop: 62,
+        padding: 24
+    },
+
+    logo: {
+        height: 34,
+        width: 134,
+    }
+
+})
